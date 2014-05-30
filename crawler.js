871aa90de982d6e08e323b9e074ea1050a13482e
shinecita <shinecita@gmail.com>
2014-05-30 16:32:40 -0400

    no message

--- crawler.js
+++ crawler.js
@@ -1,4 +1,5 @@
-var Crawler = require("crawler").Crawler;
+var Crawler = require("crawler").Crawler,
+    prompt = require("prompt");
 
 
 var c = new Crawler({
@@ -8,11 +9,19 @@ var c = new Crawler({
 "callback":function(error,result, $) {
       console.log("asd2", error)
     // $ is a jQuery instance scoped to the server-side DOM of the page
-    $("a[href^='https://twitter.com/']").each(function(index , a) {
-        console.log("ASD", index,  a.href);
+    $("a[href^='http://twitter.com/']").each(function(index , a) {
+        console.log("twitter user link", index,  a.href);
     });
    }
 });
 
 // Queue just one URL, with default callback
-c.queue("http://www.abc.com.py");
+
+ prompt.start();
+ 
+   prompt.get(['url'], function (err, result) {
+    console.log(err, result);
+    c.queue(result.url);
+  });
+ 
+
